2da09d4b3ec97faaf91dc7da698f08abdd5db2d9
jusukarneh <jusudaddi@yahoo.com>
2023-07-30 16:43:52 -0400

    save work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var healthchoice="dairy-free"
 var apiKey="7b96ce73245fd63d266ec31879968a08"
 var recipes=`https://api.edamam.com/api/recipes/v2?type=public&health=${healthchoice}&app_id=cfd01724&app_key=7b96ce73245fd63d266ec31879968a08%20%09`
+var getstartedbBtn = document.getElementById("get-started-btn")
 
 fetch(recipes, { headers: {
     'X-Api-Key': apiKey,}})
