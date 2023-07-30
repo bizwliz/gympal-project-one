937db905253feb762dd45aa77df93dc271b7fda3
The Real Mi <miaolmedillo@gmail.com>
2023-07-30 13:16:02 -0400

    added comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
 
-var healthchoice="dairy-free"
-var apiKey="7b96ce73245fd63d266ec31879968a08"
-var recipes=`https://api.edamam.com/api/recipes/v2?type=public&health=${healthchoice}&app_id=cfd01724&app_key=7b96ce73245fd63d266ec31879968a08%20%09`
+var healthchoice = "dairy-free"
+var apiKey = "7b96ce73245fd63d266ec31879968a08"
+var recipes = `https://api.edamam.com/api/recipes/v2?type=public&health=${healthchoice}&app_id=cfd01724&app_key=7b96ce73245fd63d266ec31879968a08%20%09`
 
-fetch(recipes, { headers: {
-    'X-Api-Key': apiKey,}})
-
-    .then(function (response){
-    return response.json();
+fetch(recipes, {
+    headers: {
+        'X-Api-Key': apiKey,
+    }
 })
-    .then(function (data){
-        console.log(data);
 
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+
+// TESTING API NINJAS EXERCISE API
 var APIKey = "el9fiYScbudO77M6OCsIXA==qu4fJ4psSnQw7h6M";
 var urlMuscles = "https://api.api-ninjas.com/v1/exercises?muscle="
 
@@ -19,15 +24,16 @@ var urlMuscles = "https://api.api-ninjas.com/v1/exercises?muscle="
 fetch(urlMuscles + "biceps", {
     headers: {
         'X-Api-Key': APIKey,
-    // 'contentType': 'application/json',
-    // 'cache': 'reload'
+        // 'contentType': 'application/json',
+        // 'cache': 'reload'
     }
 })
-    .then(function (response){
-    return response.json();
-})
+    .then(function (response) {
+        return response.json();
+    })
 
-    .then(function (data){
-    console.log(data);
+    .then(function (data) {
+        console.log(data);
 
-})
+    })
+
