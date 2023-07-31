8d3ae1aaf8abaeeead7fdd4db3e31ced202b93e6
The Real Mi <miaolmedillo@gmail.com>
2023-07-31 19:26:48 -0400

    committing changes to JavaScript to try working on login functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,11 +39,42 @@ fetch(urlMuscles + "biceps", {
 
 //Account creation page functions
 var signUpBtn = document.getElementById("sign-up-btn");
+var loginBtn = document.getElementById('loginBtn');
 
 function storeUserInfo() {
     var signUpName = document.getElementById("sign-up-name").value;
+    var dobInput = document.getElementById('dob').value;
+    // capture the DOB and email from html
 
-    localStorage.setItem("username", signUpName);
-}
-signUpBtn.addEventListener('click', storeUserInfo());
-console.log(signUpName);
+    // Here, we are grabbing the previous data from localStorage, if there is no data in localStorage, then this var will be an empty object
+    var userData = JSON.parse(localStorage.getItem(loggedInUser)) || {};
+
+    // Setting the keys in the object using dot notation
+    userData.name = signUpName || '';
+    userData.dob = dobInput || '';
+
+    // var arrayofObjects = [{}, {}]
+
+    localStorage.setItem(loggedInUser, JSON.stringify(userData));
+};
+
+function login() {
+    // We should have an initial form where the user types in their name and our app will look in local storage for a key with that name
+    var loginName = document.getElementById('login-name').value;
+    loggedInUser = loginName;
+
+    var foundUser = JSON.parse(localStorage.getItem(loggedInUser)) || false;
+
+    if(foundUser) {
+        var nameInput = document.getElementById('sign-up-name');
+        var dobInput = document.getElementById('dob');
+        nameInput.value = foundUser.name;
+        dobInput.value = foundUser.dob;
+    } else {
+        alert('User not found!');
+    }
+};
+
+loginBtn.addEventListener('click', login);
+
+signUpBtn.addEventListener('click', storeUserInfo);
