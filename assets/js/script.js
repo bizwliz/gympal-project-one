8dd8450ff6fab38978a9326f223f8bb5d2d35f79
The Real Mi <miaolmedillo@gmail.com>
2023-08-01 20:15:06 -0400

    added checklist input to local storage object

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+var modalCloseBtn = document.querySelector(".modal-close");
+var signUpBtn = document.getElementById("sign-up-btn");
+var errorModal = document.getElementById("error-modal");
+var userArr = [];
 var healthchoice = "dairy-free"
 var apiKey = "7b96ce73245fd63d266ec31879968a08"
 var recipes = `https://api.edamam.com/api/recipes/v2?type=public&health=${healthchoice}&app_id=cfd01724&app_key=7b96ce73245fd63d266ec31879968a08%20%09`
@@ -50,23 +54,34 @@ function hideModal() {
 
 //Account creation page functions
 // Account creation page functions
-var signUpBtn = document.getElementById("sign-up-btn");
-var errorModal = document.getElementById("error-modal");
+
 
 //TO-DO: store each user input as its own object in local storage key: UserEntryDatabase
 function storeUserInfo() {
     var signUpName = document.getElementById("sign-up-name").value;
     var signUpEmail = document.getElementById("sign-up-email").value;
     var signUpDob = document.getElementById("sign-up-dob").value;
+    var dietaryNeeds = []
+    
     var userObject = {
         name: signUpName,
         dob: signUpDob,
-        email: signUpEmail
+        email: signUpEmail,
+        diet: dietaryNeeds
     }
 
     // Check if all input fields have valid information
     if (signUpName && signUpEmail && signUpDob) {
-        localStorage.setItem("userObj", JSON.stringify(userObject));
+        for (let i = 1; i < 13; i++) {
+            var dietChoice = document.getElementById("diet" + [i]);
+            if (dietChoice.checked) {
+                dietaryNeeds.push(dietChoice.value);
+            }
+            
+        }
+        userArr.push(userObject);
+        
+        localStorage.setItem("userObj", JSON.stringify(userArr));
         // localStorage.setItem("Dob", signUpDob);
         // localStorage.setItem("Name", signUpName);
 
@@ -74,14 +89,25 @@ function storeUserInfo() {
         console.log("Email: " + signUpEmail);
         console.log("Date of Birth: " + signUpDob);
 
+        //loop through diet checklist to store dietary needs
+
+    
         // Navigate to the next page (page 3) when all fields are filled
         window.location.href = "./page5.html";
     } else {
         showModal(); // Show the modal if fields are not filled
     }
 }
+function getStorage (){
+    var storage = localStorage.getItem('userObj');
+    if(!storage){
+        userArr = [];
+        return;
+    }
+    userArr = JSON.parse(storage);
+}
 
+getStorage();
 signUpBtn.addEventListener('click', storeUserInfo);
-var modalCloseBtn = document.querySelector(".modal-close");
 modalCloseBtn.addEventListener("click", hideModal);
 
